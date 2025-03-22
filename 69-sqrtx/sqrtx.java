9b4f724cdcc8126e1d76e8dab26ede9b6404d0e7
Kirti Sharma <kirtisharma132003@gmail.com>
2025-03-22 16:53:40 +0530

    Time: 1 ms (86.17%) | Memory: 41.5 MB (6.94%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int mySqrt(int x) {
+        //  return (int)Math.sqrt(x); //newton raphson method :>fastest
+        long lo =0, hi =x;
+        while(lo<=hi){
+            long mid = lo+(hi-lo)/2;
+            if(mid*mid==(long)x) return (int)mid;
+            else if(mid*mid>(long)x) hi = mid-1;
+            else lo=mid+1;
+        }
+        return (int)hi;
+    }
+}
