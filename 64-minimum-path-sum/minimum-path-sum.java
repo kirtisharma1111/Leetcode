54974da81e5e75e199485e40d32fa1477182089b
Kirti Sharma <kirtisharma132003@gmail.com>
2025-04-26 22:54:23 +0530

    Time: 4 ms (61.73%) | Memory: 48.1 MB (9.31%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minPathSum(int[][] arr) {
+       int m = arr.length, n = arr[0].length;
+       for(int i=0; i<m; i++){
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0) continue;
+            else if(i==0) arr[i][j] += arr[i][j-1];
+            else if(j==0) arr[i][j] += arr[i-1][j];
+            else arr[i][j] += Math.min(arr[i][j-1],arr[i-1][j]);
+        }
+       }
+       return arr[m-1][n-1];
+    }
+}
